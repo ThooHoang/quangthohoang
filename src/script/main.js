7f2038631aae7cb76f31e725d25c6f1265cd4a59
Quang Tho Hoang <147547139+HoanTheG@users.noreply.github.com>
2025-09-21 01:30:43 +0200

    add typing effect and quote button; enhance header and hero section styles

--- src/script/main.js
+++ src/script/main.js
@@ -30,6 +30,56 @@ document.addEventListener('click', function(e) {
     }
 })
 
+// Typing Effect
+
+const typingElement = document.getElementById('typing-effect');
+const words = [
+    "/ FRONTEND DEVELOPER",
+    "/ WEB DESIGNER",
+    "/ UX/UI DESIGNER"
+];
+
+let wordIndex = 0;
+let charIndex = 0;
+let isDeleting = false;
 
 
+function type() {
+    const currentWord = words[wordIndex];
+    let displayText = currentWord.substring(0, charIndex);
 
+    typingElement.textContent = displayText;
+
+    if(!isDeleting && charIndex < currentWord.length) {
+        charIndex++;
+        setTimeout(type,100);
+    } else if (isDeleting && charIndex >0 ) {
+        charIndex--;
+        setTimeout(type, 50);
+    } else {
+        if(!isDeleting) {
+            isDeleting = true ;
+            setTimeout(type, 1200)
+        } else {
+            isDeleting = false;
+            wordIndex=(wordIndex+1) % words.length;
+            setTimeout(type,400)
+        }
+    }
+
+}
+
+type()
+
+
+// Quote Button
+const quoteBtn = document.getElementById('quote-btn');
+const heroQuote = document.getElementById('hero-quote');
+
+quoteBtn.addEventListener('click', function() {
+  if (heroQuote.style.display === "none" || heroQuote.style.display === "") {
+    heroQuote.style.display = "block";
+  } else {
+    heroQuote.style.display = "none";
+  }
+});
